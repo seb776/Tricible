da950a045b790484735414abdef3c1b7afdd098a
seb776 <sebastien.maire@epitech.eu>
2016-01-08 20:38:07 +0000

    SFML link ok. Window is opened.

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -3,8 +3,20 @@
 // Contribs:
 //
 
+#include <SFML/Window.hpp>
 
 int main()
 {
+	sf::Window window(sf::VideoMode(800, 600), "My window");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
     return 0;
 }
