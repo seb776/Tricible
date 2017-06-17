cb2d532e200379ba53a80941f1c2dc7c897804d7
Sebastien Maire <sebastien.maire@epitech.eu>
2017-06-17 22:07:26 +0200

    Still working on scene

---
+++ Engine/Scene/Scene.cpp
@@ -0,0 +1,10 @@
+#include "Scene.hpp"
+
+bool Tricible::Scene::IntersectsRay(const Point3 & origin, const Point3 & vec, float & dist, int & color)
+{
+	return false;
+}
+
+void Tricible::Scene::ComputeNormal(const Point3 & inter, const Point3 & incident, Point3 & normal)
+{
+}
--- Engine/Scene/Scene.hpp
+++ Engine/Scene/Scene.hpp
@@ -8,5 +8,20 @@ namespace Tricible
 	class Scene : public AObject, public AIntersectable
 	{
 		std::vector<AObject *> Objects;
+
+		bool IntersectsRays(AObject *interObj, const Point3 & origin, const Point3 & vec, float & dist)
+		{
+
+		}
+
+		// Inherited via AIntersectable
+		virtual bool IntersectsRay(const Point3 & origin, const Point3 & vec, float & dist, int & color) override
+		{
+
+		}
+		virtual void ComputeNormal(const Point3 & inter, const Point3 & incident, Point3 & normal) override
+		{
+
+		}
 	};
 }
